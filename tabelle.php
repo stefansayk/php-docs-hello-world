32d408f6ad59b020e83b20ace50b5898d49b5779
Stefan Sayk <124285137+stefansayk@users.noreply.github.com>
2023-02-03 08:58:29 +0100

    jetzt mit DB

--- tabelle.php
+++ tabelle.php
@@ -2,20 +2,19 @@
 <head> <!-- Start HTML-Kopf -->
 <!-- CSS-Formatierung für die Tabelle -->
 <style type="text/css">
- table {
- background-color: #E2E9F6;
- border-collapse: collapse;
- border: none;
- }
- thead {
- background-color: #4D79C7;
- color: #FFFFFF;
- }
-
- td, th {
- text-align: left;
- padding: 0.5em 1em;
- }
+table {
+background-color: #E2E9F6;
+border-collapse: collapse;
+border: none;
+}
+thead {
+background-color: #4D79C7;
+color: #FFFFFF;
+}
+td, th {
+text-align: left;
+padding: 0.5em 1em;
+}
 </style> <!-- Ende CSS-Style -->
 </head> <!-- Ende HTML-Kopf -->
 <body> <!-- Start HTML-Body -->
@@ -24,12 +23,19 @@
 $bnr = $_GET["bnr"];
 echo "<h1>iBrot-Bestellung</h1>";
 echo "<h2>Bestellnummer: $bnr </h2>";
-/* Daten in einem assoziativen Array definieren*/
-$daten = array (
- array("Position"=>1,"ArtikelID"=>"Brötchen","Menge"=>5),
- array("Position"=>2,"ArtikelID"=>"Croissant","Menge"=>3),
- array("Position"=>3,"ArtikelID"=>"Brot","Menge"=>1)
- );
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "say", "pwd" => 'Azure4bschule', "Database" => "ibrot", "LoginTimeout" => 30,
+"Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:xxx-ibrot-dbsrv.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+/* Set up and execute the query. */
+$tsql = "SELECT * FROM Bestellposition WHERE BestellungID=$bnr";
+$stmt = sqlsrv_query( $conn, $tsql);
+if( $stmt === false)
+{
+echo "Error in query preparation/execution.\n";
+die( print_r( sqlsrv_errors(), true));
+}
 /* Überschriften im Tabellen-Head ausgeben */
 echo "<table>";
 echo "<thead>";
@@ -41,16 +47,20 @@ echo "</tr>";
 echo "</thead>";
 /* Datensätze im Tabellen-Body ausgeben */
 echo "<tbody>";
-foreach ($daten as $row) {
- echo "<tr>";
- echo "<td>".$row['Position']."</td>";
- echo "<td>".$row['ArtikelID']."</td>";
- echo "<td>".$row['Menge']."</td>";
- echo "</tr>";
+/* Retrieve each row as an associative array and display the results.*/
+while( $row = sqlsrv_fetch_array( $stmt, SQLSRV_FETCH_ASSOC))
+{
+echo "<tr>";
+echo "<td>".$row['Position']."</td>";
+echo "<td>".$row['ArtikelID']."</td>";
+echo "<td>".$row['Menge']."</td>";
+echo "</tr>";
 }
 echo "</table>";
 echo "<tbody>";
-
+/* Free statement and connection resources. */
+sqlsrv_free_stmt( $stmt);
+sqlsrv_close( $conn);
 ?> <!-- Ende PHP-->
 </body> <!-- Ende HTML-Body-->
 </html> <!-- Ende HTML-->
